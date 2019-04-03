361df3fbb6b00b0b1ffaf7bb7d70aaa8e5a78752
Marius <marius@twostairs.com>
2019-04-03 22:58:17 +0200

    2 spaces doesn’t work for my old eyes, sorry

--- mix.exs
+++ mix.exs
@@ -1,37 +1,37 @@
 defmodule Paperwork.Users.MixProject do
-  use Mix.Project
+    use Mix.Project
 
-  def project do
-    [
-      app: :paperwork_service_users,
-      version: "0.1.0",
-      elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
+    def project do
+        [
+            app: :paperwork_service_users,
+            version: "0.1.0",
+            elixir: "~> 1.8",
+            start_permanent: Mix.env() == :prod,
+            deps: deps()
+        ]
+    end
 
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {Paperwork.Application, []}
-    ]
-  end
+    def application do
+        [
+            extra_applications: [:logger],
+            mod: {Paperwork.Application, []}
+        ]
+    end
 
-  defp deps do
-    [
-      # {:paperwork, path: "../paperwork.ex"},
-      {:paperwork, git: "https://github.com/paperwork/paperwork.ex.git"},
-      {:maru, "~> 0.14.0-pre.1"},
-      {:plug_cowboy, "~> 2.0"},
-      {:jason, "~> 1.1"},
-      {:corsica, "~> 1.1"},
-      {:mongodb, "~> 0.4.7"},
-      {:poolboy, "~> 1.5"},
-      {:bcrypt_elixir, "~> 2.0"},
-      {:joken, "~> 2.0"},
-      {:distillery, "~> 2.0"},
-      {:exsync, "~> 0.2", only: :dev}
-    ]
-  end
+    defp deps do
+        [
+            # {:paperwork, path: "../paperwork.ex"},
+            {:paperwork, git: "https://github.com/paperwork/paperwork.ex.git"},
+            {:maru, "~> 0.14.0-pre.1"},
+            {:plug_cowboy, "~> 2.0"},
+            {:jason, "~> 1.1"},
+            {:corsica, "~> 1.1"},
+            {:mongodb, "~> 0.4.7"},
+            {:poolboy, "~> 1.5"},
+            {:bcrypt_elixir, "~> 2.0"},
+            {:joken, "~> 2.0"},
+            {:distillery, "~> 2.0"},
+            {:exsync, "~> 0.2", only: :dev}
+        ]
+    end
 end
