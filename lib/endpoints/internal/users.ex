515d0eb2d74e2d8d7b96a50f2265d27f0a004d98
Marius <marius@twostairs.com>
2019-05-25 11:37:06 +0200

    Refactored responses

--- lib/endpoints/internal/users.ex
+++ lib/endpoints/internal/users.ex
@@ -8,17 +8,19 @@ defmodule Paperwork.Users.Endpoints.Internal.Users do
     namespace :internal do
         namespace :users do
             get do
-                {:ok, users} = Paperwork.Collections.User.list()
+                response = Paperwork.Collections.User.list()
+
                 conn
-                |> resp({:ok, users})
+                |> resp(response)
             end
 
             route_param :id do
                 get do
                     id = Paperwork.Id.from_gid(params[:id])
-                    {:ok, user} = id |> Paperwork.Collections.User.show
+                    response = id |> Paperwork.Collections.User.show
+
                     conn
-                    |> resp({:ok, user})
+                    |> resp(response)
                 end
             end
         end
