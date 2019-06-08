48eb912446e2e239f613cc53d33e31fa0769c566
Marius <marius@twostairs.com>
2019-06-08 12:19:39 +0200

    Added Paperwork.Events.Publisher as child

--- lib/application.ex
+++ lib/application.ex
@@ -12,7 +12,8 @@ defmodule Paperwork.Users.Application do
         children = [
             Paperwork.Ex,
             Paperwork.Users.Server,
-            {Mongo, [name: :mongo, url: Confex.fetch_env!(:paperwork, :mongodb)[:url], pool: DBConnection.Poolboy]}
+            {Mongo, [name: :mongo, url: Confex.fetch_env!(:paperwork, :mongodb)[:url], pool: DBConnection.Poolboy]},
+            Paperwork.Events.Publisher
         ]
 
         opts = [strategy: :one_for_one, name: Paperwork.Users.Supervisor]
