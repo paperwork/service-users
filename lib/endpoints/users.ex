07a2c78cf481c81e2b11de9c7bfd5c07314f8869
Marius <marius@twostairs.com>
2019-06-08 12:20:41 +0200

    Implemented journaling, implemented journals endpoint

--- lib/endpoints/users.ex
+++ lib/endpoints/users.ex
@@ -1,4 +1,5 @@
 defmodule Paperwork.Users.Endpoints.Users do
+    require Logger
     use Paperwork.Users.Server
     use Paperwork.Helpers.Response
 
@@ -82,24 +83,84 @@ defmodule Paperwork.Users.Endpoints.Users do
                 session_user_id =
                     conn
                     |> Paperwork.Auth.Session.get_paperwork_id()
+
                 session_user_role =
                     conn
                     |> Paperwork.Auth.Session.get_user_role()
+
                 update_user_id =
                     params[:id]
                     |> Paperwork.Id.from_gid()
+                    |> IO.inspect
 
                 response = cond do
-                    session_user_id != update_user_id and session_user_role != :role_admin -> {:unauthorized, %{status: 0, content: %{error: "Not authorized to update other users"}}}
-                    true -> struct(Paperwork.Collections.User, params)
+                    session_user_id != update_user_id and session_user_role != :role_admin ->
+                        {:unauthorized, %{status: 0, content: %{error: "Not authorized to update other users"}}}
+                    true ->
+                        struct(Paperwork.Collections.User, params)
                             |> Map.put(:id, update_user_id.id)
                             |> Map.put(:updated_at, DateTime.utc_now())
                             |> Paperwork.Collections.User.update
+                            |> Paperwork.Helpers.Journal.api_response_to_journal(
+                                case params |> Map.has_key?(:password) do
+                                    true -> params |> Map.put(:password, "CENSORED")
+                                    false -> params
+                                end, :update, :user, :user, session_user_id.gid, [session_user_id])
                 end
 
                 conn
                 |> resp(response)
             end
+
+            namespace :journals do
+                params do
+                    optional :resource,           type: String
+                    optional :newer_than_id,      type: String
+                end
+                get do
+                    session_user_id =
+                        conn
+                        |> Paperwork.Auth.Session.get_paperwork_id()
+
+                    session_user_role =
+                        conn
+                        |> Paperwork.Auth.Session.get_user_role()
+
+                    show_user_id =
+                        params[:id]
+                        |> Paperwork.Id.from_gid()
+
+                    response = cond do
+                        session_user_id != show_user_id and session_user_role != :role_admin ->
+                            {:unauthorized, "Not authorized to see other user's journals!"}
+                        true ->
+                            journals_params =
+                                %{
+                                    relevant_to: show_user_id.gid,
+                                    resource: params |> Map.get(:resource),
+                                    newer_than_id: params |> Map.get(:newer_than_id)
+                                }
+
+                            with \
+                                {:ok, _} <- Paperwork.Id.validate_gid(show_user_id.gid),
+                                {:ok, journals} <- Paperwork.Internal.Request.journals(journals_params)
+                            do
+                                {:ok, journals}
+                            else
+                                {:notfound, _} ->
+                                    Logger.warn("Internal request returned 404")
+                                    {:notfound, ""}
+                                other ->
+                                    Logger.error("Validation of access GID failed: #{inspect other}")
+                                    {:badrequest, "Supplied GID (#{inspect show_user_id}) seems to be invalid"}
+                            end
+                    end
+
+                    conn
+                    |> resp(response)
+                end
+            end
+
         end
     end
 end
