f5cd3364c474745cf408191ad9467ff5f47c3693
Marius <marius@twostairs.com>
2019-06-08 12:19:48 +0200

    Added events config

--- config/config.exs
+++ config/config.exs
@@ -27,6 +27,11 @@ config :paperwork, :internal,
     attachments: {:system, :string, "INTERNAL_RESOURCE_ATTACHMENTS", "http://localhost:8883/internal/attachments"},
     journals:    {:system, :string, "INTERNAL_RESOURCE_JOURNALS",    "http://localhost:8884/internal/journals"}
 
+config :paperwork, :events,
+    url: {:system, :string, "EVENTS_URL", "amqp://localhost"},
+    reconnect_interval: {:system, :integer, "EVENTS_RECONNECT_INTERVAL", 10_000},
+    exchange: {:system, :string, "EVENTS_EXCHANGE", "journals_exchange"}
+
 config :logger,
     backends: [:console],
     level: :debug
